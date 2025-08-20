e24cc39cf8af75ce9584a204be4f5a7d3916833a
jamesdai05 <dai_guilong@hotmail.com>
2025-08-21 01:23:57 +0800

    (feat):add the navlinks for small screen

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,9 +1,35 @@
 import Wrapper from "./wrapper/sidebar.js"
+import Logo from './Logo';
+import { useDashboardContext } from "../pages/Admin/DashboardLayout.jsx";
+import Navlinks from "./Navlinks.jsx";
+import { FaTimes } from "react-icons/fa";
+// import { useState } from "react";
+
 
 const Sidebar = () => {
-  return (
+  const {showSidebar, toggleSidebar } = useDashboardContext();
+  const sideBarClassName = showSidebar
+      ? "show-sidebar sidebar-container"
+      : "sidebar-container";
 
-    <Wrapper>sidebar</Wrapper>
-  )
+  return (
+      <Wrapper>
+          <div className={sideBarClassName}>
+              <div className="content">
+                  <button
+                      type="button"
+                      className="close-btn"
+                      onClick={toggleSidebar}
+                  >
+                      <FaTimes />
+                  </button>
+                  <header>
+                    <Logo />
+                  </header>
+                  <Navlinks />
+              </div>
+          </div>
+      </Wrapper>
+  );
 }
 export default Sidebar
--- frontend/src/components/wrapper/sidebar.js
+++ frontend/src/components/wrapper/sidebar.js
@@ -1,21 +1,72 @@
 import styled from "styled-components";
 
 const Wrapper=styled.aside`
+    .sidebar-container{
+      position:fixed;
+      inset:0;  /* for the fixed position display, to adjust the position*/
+      background-color: rgba(0,0,0,0.7);
+      display:flex;
+      justify-content:center;
+      align-items:center;
+      z-index:0;
+      transition: var(--transition);
+      visibility:hidden;
+      opacity:0;
+    }
+    .show-sidebar {
+      z-index: 99;
+      opacity: 1;
+      visibility: visible;
+    }
+    .content {
+      background: var(--background-secondary-color);
+      width: var(--fluid-width);
+      height: 95vh;
+      border-radius: var(--border-radius);
+      padding: 4rem 2rem;
+      position: relative;
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+    }
+    .close-btn {
+      position: absolute;
+      top: 10px;
+      left: 10px;
+      background: transparent;
+      border-color: transparent;
+      font-size: 2rem;
+      color: var(--red-dark);
+      cursor: pointer;
+    }
+    .nav-links {
+      padding-top: 2rem;
+      display: flex;
+      flex-direction: column;
+    }
+    .nav-link {
+      display: flex;
+      align-items: center;
+      color: var(--text-secondary-color);
+      padding: 1rem 0;
+      text-transform: capitalize;
+      transition: var(--transition);
+    }
+    .nav-link:hover {
+      color: var(--primary-500);
+    }
+    .icon {
+      font-size: 1.5rem;
+      margin-right: 1rem;
+      display: grid;
+      place-items: center;
+    }
+    .active {
+      color: var(--primary-500);
+    }
     @media (min-width: 992px) {
         display: none;
-  }
-
-  .sidebar-container{
-    position:fixed;
-    inset:0;  /* for the fixed position display, to adjust the position*/
-    background-color: rgba(0,0,0,0.7);
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    z-index:-1;
-    transition:var(--transition);
-    visibility:hidden;
-  }
+    }
 `
 
 export default Wrapper;
