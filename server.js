35a1b433f6c8d281be2157c7506676907bd104a3
jamesdai05 <dai_guilong@hotmail.com>
2025-08-21 22:17:17 +0800

    set up the basic server for backend

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
+const app=express();
+
+dotenv.config()
+
+const port= process.env.PORT || 4000 
+
+
+
+
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(morgan("dev"));
+
+
+app.get("/", (req,res)=>{
+    res.send("Hello world")
+})
+
+app.post("/",(req,res)=>{
+    console.log(req.body.title)
+    res.send("Post request submitted.")
+})
+
+
+app.listen(port,()=>{
+    console.log("Server is running on port",port)
+})
