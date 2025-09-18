8de0a5e9033493dd180a0e3159e5ba408cd414aa
ThePrimeagen <dai_guilong@hotmail.com>
2025-09-18 23:01:52 +0800

    feat:add customized error

--- middleWare/errorMiddleware.js
+++ middleWare/errorMiddleware.js
@@ -41,7 +41,16 @@ const errorHandler = (err, req, res, next) => {
   });
 };
 
+class BadRequestError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'BadRequestError';
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
 export {
     errorHandler,
     notFound,
+    BadRequestError
 }
